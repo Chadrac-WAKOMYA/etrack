ddb73daff15585794084d6fe228ab0e0d502fa23
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-06-09 05:20:56 +0200

    TEST second initial test sucess

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <button className="btn btn-accent">Test</button>
+    <div>
+      <div className="flex items-center justify-center flex-col py-10 w-full">
+        <div>
+          <div className="flex flex-col">
+            <h1 className="text-4xl md:text-5xl font-bold text-center">
+              Prenez le contrôle <br /> de vos finances
+            </h1>
+          </div>
+        </div>
+      </div> 
+    </div>
   );
 }
