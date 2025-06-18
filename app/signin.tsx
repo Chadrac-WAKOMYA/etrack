d6ecb9b4475b96b24413ab593f22b13e5ed2308d
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-06-18 21:16:53 +0200

    sign in pagE UPDATED

--- app/signin.tsx
+++ app/signin.tsx
@@ -2,9 +2,9 @@
 import { SignIn, useUser } from '@clerk/nextjs'
 
 export default function Home() {
-  const { user } = useUser()
+//   const { user } = useUser()
 
-  if (!user) return <SignIn />
+//   if (!user) return <SignIn />
 
   return <div>Welcome!</div>
 }
